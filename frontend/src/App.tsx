7ec997c10bc790346c2dc317caac33f160256748
Tanish <tanishtp01@gmail.com>
2025-03-08 23:13:36 +0530

    Navbar work done..!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import Navbar from '../pages/navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
-      <h1 className='text-2xl font-bold underline text-center mt-2'>Test for Tailwindcss</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+      </Routes>
+      <h1 className='text-2xl font-bold underline text-center mt-12'>Test for Tailwindcss</h1>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
