d2eb09c4025d29408098af9b5c13649e34e2014e
Dipesh Kafle <dipeshkaflen2060@gmail.com>
2025-03-11 14:16:03 +0530

    Added Interview Experience schema!!

--- backend/model/InterviewExp.js
+++ backend/model/InterviewExp.js
@@ -1,6 +1,41 @@
 const mongoose = require("mongoose");
 
-const interviewSchema = new mongoose.Schema({});
+const interviewSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    interviewType: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    questions: {
+      dsa: [{ name: String }],
+      theory: [{ question: String }],
+      queries: [{ question: String }],
+    },
+    difficultyLevel: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const InterviewExp = new mongoose.model("interviews", interviewSchema);
 
