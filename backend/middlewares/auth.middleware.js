399339085a51fd1b631c26daea5a53a9636e5505
Dipesh Kafle <dipeshkaflen2060@gmail.com>
2025-03-16 11:13:50 +0530

    Update middleware and working fine!!

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,19 +2,25 @@ const jwt = require("jsonwebtoken");
 
 const protectedRoute = async (req, res, next) => {
   try {
-    let token = req.headers["authorization"].split(" ")[1];
+    const header = req.headers["authorization"];
+    if (!header) {
+      return res
+        .status(404)
+        .json({ status: false, msg: "Token isn't in header" });
+    }
+    const token = header.split(" ")[1];
     if (!token) {
       return res
         .status(404)
         .json({ status: false, msg: "Token is not provided" });
     }
-    const { name, email } = jwt.verify(token, process.env.JWT_SECRET);
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.email = user.email;
+    next();
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ status: false, msg: "Token is expired" });
   }
-  // include email into the req object
-  req.email = email;
-  next();
 };
 
 module.exports = { protectedRoute };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,9 +3,14 @@ const {
   handleUserLogin,
   handleUserRegister,
 } = require("../controllers/user.controllers");
+const { isAdmin } = require("../middlewares/admin.middleware");
+const { protectedRoute } = require("../middlewares/auth.middleware");
 const user = express.Router();
 
 user.post("/login", handleUserLogin);
 user.post("/register", handleUserRegister);
+user.get("/check", protectedRoute, isAdmin, (req, res) => {
+  res.json({ msg: "Successfully passed test" });
+});
 
 module.exports = { user };
