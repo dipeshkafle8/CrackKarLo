f50c65a13a560ac5e4b4e825b0aa4541413277f0
Dipesh Kafle <dipeshkaflen2060@gmail.com>
2025-03-16 17:34:40 +0530

    Added create new course endpoint

--- backend/controllers/course.controllers.js
+++ backend/controllers/course.controllers.js
@@ -2,6 +2,9 @@ const { Course } = require("../model/course.model");
 const { Module } = require("../model/module.model");
 const { Question } = require("../model/question.model");
 
+// --------------------- FOR GETTING DATAS ------------------------
+
+// for getting all Courses
 const getAllCourses = async (req, res) => {
   try {
     const courses = await Course.find({});
@@ -9,10 +12,12 @@ const getAllCourses = async (req, res) => {
       .status(200)
       .json({ status: true, msg: "Courses fetched", Courses: courses });
   } catch (err) {
+    console.log("Error in getting Courses", err);
     res.status(500).json({ status: false, msg: "Error in getAllCourse" });
   }
 };
 
+//for getting modules on particular courses
 const getAllModules = async (req, res) => {
   try {
     const { courseId } = req.params;
@@ -21,11 +26,48 @@ const getAllModules = async (req, res) => {
       .status(200)
       .json({ status: true, msg: "modules fetched", Modules: modules });
   } catch (err) {
+    console.log("Error in getting Modules", err);
     res.status(500).json({ status: false, msg: "Error in getting modules" });
+  }
+};
+
+//for getting All questions on a particular module
+
+const getAllQuestions = async (req, res) => {
+  try {
+    const { moduleId } = req.params;
+    const questions = await Question.find({ module: moduleId });
+    res
+      .status(200)
+      .json({ status: true, msg: "Questions fetched", Questions: questions });
+  } catch (err) {
+    console.log("Error in getting Questions", err);
+    res.status(500).json({ status: true, msg: "Error in getting questions" });
+  }
+};
+
+//-------------------------FOR INSERTING DATA---------------------------------
+
+//for creating new Course only allowed to admin
+const handleCreateCourse = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newCourse = new Course({
+      title,
+      description,
+    });
+    newCourse.save();
+    res
+      .status(200)
+      .json({ status: true, msg: "Course created", id: newCourse._id });
+  } catch (err) {
+    console.log("Error in creating Course", err);
+    res.status(500).json({ status: false, msg: "Error in Creating Course" });
   }
 };
 
 module.exports = {
   getAllCourses,
   getAllModules,
+  getAllQuestions,
 };
--- backend/routes/course.route.js
+++ backend/routes/course.route.js
@@ -1,5 +1,13 @@
 const express = require("express");
-
+const {
+  getAllCourses,
+  getAllModules,
+  getAllQuestions,
+} = require("../controllers/course.controllers");
 const course = express.Router();
+
+course.get("/allCourses", getAllCourses);
+course.get("/getModules/:courseId", getAllModules);
+course.get("/getQuestions/:moduleId", getAllQuestions);
 
 module.exports = { course };
